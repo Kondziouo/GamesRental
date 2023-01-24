3c5a78d0d274eeb2969174705f16b2237918088a
Konrad <balo271@gmail.com>
2023-01-24 13:50:35 +0100

    Change

--- GamesRental/Controllers/UserController.cs
+++ GamesRental/Controllers/UserController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GamesRental.Data;
+using Microsoft.AspNetCore.Mvc;
+using GamesRental.Models;
 
 namespace GamesRental.Controllers
 {
     public class UserController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Customer> customers = _context.Customers.ToList();
+            return View(customers);
         }
     }
 }
