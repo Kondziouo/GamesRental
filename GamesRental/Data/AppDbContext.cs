bf85757f8fd12532ca21772f1e432239ad7c79ac
Konrad <balo271@gmail.com>
2023-01-27 18:17:25 +0100

    small graphic change

--- GamesRental/Data/AppDbContext.cs
+++ GamesRental/Data/AppDbContext.cs
@@ -17,21 +17,5 @@ namespace GamesRental.Data
         public DbSet<Game> Games { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.ApplyConfiguration(new ApplcationUserEntityConfigruation());
-        }
     }
-    public class ApplcationUserEntityConfigruation : IEntityTypeConfiguration<Customer>
-    {
-        public void Configure(EntityTypeBuilder<Customer> builder)
-        {
-            builder.Property(u => u.FirstName).HasMaxLength(255);
-            builder.Property(u => u.LastName).HasMaxLength(255);
-
-        }
-    }
-
 }
